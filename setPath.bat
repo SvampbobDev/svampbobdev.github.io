35c7500c0e906bc3e62bc74b113aad1992c79063
Svampbob452 <erictelliavestin@hotmail.com>
2022-05-19 12:53:04 +0200

    installed bulmacss

---
+++ setPath.bat
@@ -0,0 +1,3 @@
+@cd C:\Users\eritel1123\OneDrive - Stiftelsen Stockholms Stadsmission\Skrivbordet\code\svampbobdev.github.io
+@set PATH=C:\Users\eritel1123\OneDrive - Stiftelsen Stockholms Stadsmission\Skrivbordet\code\nodeJS\node-v16.15.0-win-x64;%PATH%
+@cmd.exe /K
